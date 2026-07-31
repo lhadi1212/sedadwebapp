97abf5de3d8d607a106218a130674b9c6025f97c
lhadi1212 <lhadihame@gmail.com>
2026-07-31 15:10:23 +0000

    Add files via upload

--- src/app/adminservice.ts
+++ src/app/adminservice.ts
@@ -1,13 +1,25 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Environment } from './Environment/environment';
 import { Userinfo } from './Interfaces/userinfo';
+import { Pagination } from './Interfaces/Pagination';
 @Injectable({
   providedIn: 'root',
 })
 export class Adminservice {
   constructor(private http:HttpClient){}
-  GetAllUsers(){
-   return this.http.get<Userinfo[]>(Environment.api+'api/Admin/users')
+GetAllUsers(pageNumber: number, pageSize: number = 2) {
+
+    const params = new HttpParams()
+      .set('pageNumber', pageNumber)
+      .set('pageSize', pageSize);
+
+    return this.http.get<Pagination<Userinfo>>(
+      Environment.api + 'api/Admin/users',
+      { params }
+    );
+  }
+    DeleteAllUsers(){
+   return this.http.delete<Userinfo[]>(Environment.api+'api/Admin/users')
   }
 }
