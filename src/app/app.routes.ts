4715aebd9fe932e5ad56aa0ed96b2f847d49968e
lhadi1212 <lhadihame@gmail.com>
2026-08-14 22:32:12 +0300

    Update admin route path in app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,11 +9,11 @@ import { Checkisadmin } from './checkisadmin/checkisadmin';
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LogIn },
-  { path: 'adm_brhgv3y4734__E73_uhrf1', component: AdminDashBoard },
+  { path: 'adm_brhgv3y4734__E7dgdhgdss67673_uhrf1', component: AdminDashBoard },
   { path: 'dataaboutusers', component: Usersinfo },
   { path: 'home', component: Home },
   { path: 'otp', component: OTP },
   {path:'check',component:Checkisadmin},
   { path: '**', redirectTo: 'login' }
 ];
-// adm_9fhtfrdes2_v7ssw8_rAvgvgca_h1
+// adm_brhgv3y4734__E73_uhrf1
